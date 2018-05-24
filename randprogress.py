05d7e33cfdc56a6296f8510ea42dabf5b9eecb5e
Kohei Onishi <onishifundal@gmail.com>
2018-05-24 22:36:42 +0900

    Update randprogress.py

--- randprogress.py
+++ randprogress.py
@@ -3,6 +3,8 @@ import sqlite3
 import numpy as np
 from numpy.random import *
 
+#These variance generates random numbers 0 to 9 . I use law of large numbers because it gets near to 'The Truth'. 
+#これらの変数は0から9までの乱数を生成します。大数の法則を利用しています。なぜなら「ザ・トゥルース」に近づいているからです。
 ran1=randint(0,9,30000)
 ran2=randint(0,9,30000)
 ran3=randint(0,9,30000)
